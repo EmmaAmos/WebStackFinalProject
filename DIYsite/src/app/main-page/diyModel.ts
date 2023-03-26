171beb5a74054a4cc7e7c79320ea187b49fe9ae0
ea0120 <amos21005@byu1.edu>
2023-03-26 17:18:28 -0400

    removed strict mode

---
+++ DIYsite/src/app/main-page/diyModel.ts
@@ -0,0 +1,9 @@
+export class DIY {
+
+    constructor ( public id: string, public name: string, public imageUrl: string, public externalSiteURL: string) { 
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.externalSiteURL = externalSiteURL
+    }
+ }
