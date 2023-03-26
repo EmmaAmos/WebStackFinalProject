f39c92922487d1d015fb8c8605702e617e78fabf
ea0120 <amos21005@byu1.edu>
2023-03-26 18:59:06 -0400

    added foldes for node server

--- DIYsite/src/app/app-routing.module.ts
+++ DIYsite/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DIYdetailComponent } from './main-page/diydetail/diydetail.component';
+import { DIYeditComponent } from './main-page/diyedit/diyedit.component';
+import { DIYitemComponent } from './main-page/main-diylist/diyitem/diyitem.component';
+import { MainDIYListComponent } from './main-page/main-diylist/main-diylist.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main-page', pathMatch:'full' },
+  { path: 'main-page', component: MainPageComponent, children:[ 
+    {path: 'newDIY', component: DIYdetailComponent}, {path: 'new', component: DIYeditComponent}, {path: ':id', component: DIYdetailComponent}, {path: ':id/edit', component: DIYeditComponent} 
+  ]},
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
